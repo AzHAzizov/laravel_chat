9cde26aed46967fccf60f95cf6456df026bea574
Azizov Aziz <azjhazizov@gmail.com>
2023-12-03 19:51:44 +0100

    After first lesson

---
+++ resources/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+        <router-link to="/">Main page</router-link>
+        <router-link to="about">About page</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
